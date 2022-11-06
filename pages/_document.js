1cac76881137dd12d901b7dbb3f051f94e300f08
dominic crosby <domcrosby1@gmail.com>
2022-11-06 14:16:00 -0800

    initial commit

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Mazarine Consulting</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
